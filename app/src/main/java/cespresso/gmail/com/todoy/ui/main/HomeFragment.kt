b263b13abe2d55d765ff48ae856ce2512d79c024
Noguchi <noguchi@via-at.com>
2019-03-15 21:17:21 +0900

    make base navigation and auth

--- app/src/main/java/cespresso/gmail/com/todoy/ui/main/HomeFragment.kt
+++ app/src/main/java/cespresso/gmail/com/todoy/ui/main/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import cespresso.gmail.com.todoy.R
 
 class HomeFragment :Fragment(){
@@ -15,5 +17,13 @@ class HomeFragment :Fragment(){
     ): View? {
         setHasOptionsMenu(true)
         return inflater.inflate(R.layout.home_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val buttonLogout = view.findViewById<Button>(R.id.button2)
+        buttonLogout.setOnClickListener{
+            findNavController().navigate(R.id.action_global_loginFragment)
+        }
     }
 }
--- app/src/main/java/cespresso/gmail/com/todoy/ui/main/MainActivity.kt
+++ app/src/main/java/cespresso/gmail/com/todoy/ui/main/MainActivity.kt
@@ -3,53 +3,88 @@ package cespresso.gmail.com.todoy.ui.main
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
+import android.support.v4.app.FragmentManager
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.graphics.drawable.DrawerArrowDrawable
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import cespresso.gmail.com.todoy.R
 import kotlinx.android.synthetic.main.activity_main.*
+import android.animation.ObjectAnimator
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import cespresso.gmail.com.todoy.R
+
+
 //import kotlinx.android.synthetic.main.app_bar_main.*
 
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity()/*, NavigationView.OnNavigationItemSelectedListener*/ {
 
     private lateinit var appBarConfiguration : AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(cespresso.gmail.com.todoy.R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         //
         val host: NavHostFragment = supportFragmentManager
-            .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
+            .findFragmentById(cespresso.gmail.com.todoy.R.id.my_nav_host_fragment) as NavHostFragment? ?: return
         // Set up Action Bar
         val navController = host.navController
+        // 現在表示されているfragmentの監視
+        // これは今までの方法
+        host.childFragmentManager.addOnBackStackChangedListener {
+            val drawer = supportFragmentManager.backStackEntryCount == 0
+            host.childFragmentManager.fragments[0]?.let {
+                when(it){
+                    is HomeFragment ->{
+                        fab.show()
+                    }
+                    is AddFragment->{
+                        actionBar?.hide()
+                        actionBar?.let { actionBar ->
+                            actionBar.hide()
+                            Log.i("^v^","隠されました")
+                        }
+
+                        fab.hide()
+                    }
+                    else->{
+                        fab.hide()
+                    }
+                }
+                Log.i("^v^",it.toString())
+            }
+        }
+        // navigationControllerでDestinationが変化したときに呼ばれるリスナー
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+//            supportActionBar?.title = destination.label
+//            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
 
         appBarConfiguration = AppBarConfiguration(navController.graph)
 
-        // ここよくわからない
+
+        // toolbarの設定
         val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-//        appBarConfiguration = AppBarConfiguration(
-//            setOf(R.id.home_dest,R.id.deeplink_dest),
-//            drawerLayout
-//        )
-
-        val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.mainFragment),
+            drawerLayout
         )
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
 
-        nav_view.setNavigationItemSelectedListener(this)
+        // 行先にlabelを表示する。←と🍔アイコンを出し分ける
+        toolbar.setupWithNavController(navController,appBarConfiguration)
+        nav_view.setupWithNavController(navController)
+
 
         // Global Actionでの遷移
         // TODO 現在のtopのfragmentによって表示を出し分ける方法の検討
@@ -59,54 +94,4 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     }
 
-    override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
-            }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
-
-            }
-        }
-
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
-    }
 }
