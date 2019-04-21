addb448a2aca4a383bd4768bdacd5c6a5575530a
Cespresso <2cespresso@gmail.com>
2019-04-21 16:22:44 +0900

    workerの追加

---
+++ app/src/main/java/cespresso/gmail/com/todoy/worker/GetAllTodosWorker.kt
@@ -0,0 +1,11 @@
+package cespresso.gmail.com.todoy.worker
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+//class GetAllTodosWorker(ctx:Context,params:WorkerParameters) :Worker(ctx,params){
+//    override fun doWork(): Result {
+//
+//    }
+//}
